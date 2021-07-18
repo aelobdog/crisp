d7591043c06c15de026932fad56c88c522f60c4d
Ashwin Godbole <dev.godboleashwin@gmail.com>
2021-07-18 22:13:37 +0530

    fixed some instructions

--- src/crisp.c
+++ src/crisp.c
@@ -22,11 +22,13 @@
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_stdinc.h>
 #include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_video.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define WIDTH 64
 #define HEIGHT 32
@@ -38,20 +40,27 @@ typedef uint16_t u16;
 typedef int16_t i16;
 typedef uint32_t u32;
 
-struct chip {
-  u8 v[16];
-  i8 sp;
-  u16 i;
-  u16 pc;
-  u8 mem[4096];
-  u16 stack[16];
-  u16 dtimer;
-  u16 stimer;
-  u8 display[HEIGHT * WIDTH];
-  u8 key_pressed[16];
-  i8 prev_key;
+enum key {
+   k0,k1,k2,k3,
+   k4,k5,k6,k7,
+   k8,k9,ka,kb,
+   kc,kd,ke,kf,
+   none
 };
+typedef enum key Key;
 
+struct chip {
+   u8 v[16];
+   i8 sp;
+   u16 i;
+   u16 pc;
+   u8 mem[4096];
+   u16 stack[16];
+   u16 dtimer;
+   u16 stimer;
+   u8 display[HEIGHT * WIDTH];
+   Key kp;
+};
 typedef struct chip Chip;
 typedef void (*instrPointer)(Chip *, u16);
 
@@ -84,13 +93,15 @@ drawPixel(u8 x, u8 y, SDL_Surface *surf) {
 }
 
 void
-refreshScreen(Chip *c, SDL_Surface *surf) {
-  int j;
-  for (j = 0; j < WIDTH * HEIGHT; j++) {
-    if (c->display[j]) {
-      drawPixel(j%WIDTH, j/WIDTH, surf);
-    }
-  }
+refreshScreen(Chip *c, SDL_Surface *surf, SDL_Window *window) {
+   int j;
+   SDL_FillRect(surf, NULL, SDL_MapRGB(surf->format, 0x00, 0x00, 0x00));
+   for (j = 0; j < WIDTH * HEIGHT; j++) {
+      if (c->display[j]) {
+         drawPixel(j%WIDTH, j/WIDTH, surf);
+      }
+   }
+   SDL_UpdateWindowSurface(window);
 }
 
 /* instruction set implementation ========================= */
@@ -99,19 +110,19 @@ OP_0NNN(Chip *c, u16 opcode) {}
 
 void
 OP_00E0(Chip *c, u16 opcode) {
-  int i;
-  for (i = 0; i < WIDTH * HEIGHT; i++) {
-    c->display[i] = (u8)0;
-  }
+   int i;
+   for (i = 0; i < WIDTH * HEIGHT; i++) {
+      c->display[i] = (u8)0;
+   }
 }
 
 void 
 OP_00EE(Chip *c, u16 opcode) {
-  if (c->sp < -1 || c->sp > 15) {
-    printf("ERROR: stack bounds exceeded. Stopping machine.");
-  }
-  c->pc = c->stack[c->sp--];
-  exit(1);
+   if (c->sp < 0) {
+      printf("ERROR: stack bounds exceeded. Stopping machine.\n");
+      exit(1);
+   }
+   c->pc = c->stack[c->sp--];
 }
 
 void 
@@ -119,102 +130,106 @@ OP_1NNN(Chip *c, u16 opcode) { c->pc = getAddr(opcode); }
 
 void 
 OP_2NNN(Chip *c, u16 opcode) {
-  if (c->sp < -1 || c->sp > 15) {
-    printf("ERROR: stack bounds exceeded. Stopping machine.");
-    exit(1);
-  }
-  c->stack[++c->sp] = c->pc;
-  c->pc = getAddr(opcode);
+   if (c->sp > 16) {
+      printf("ERROR: stack bounds exceeded. Stopping machine.\n");
+      exit(1);
+   }
+   c->stack[++c->sp] = c->pc;
+   c->pc = getAddr(opcode);
 }
 
 void 
 OP_3XNN(Chip *c, u16 opcode) {
-  if (c->v[getReg1(opcode)] == get8BitVal(opcode))
-    c->pc += 2;
+   if (c->v[getReg1(opcode)] == get8BitVal(opcode))
+      c->pc += 2;
 }
 
 void 
 OP_4XNN(Chip *c, u16 opcode) {
-  if (c->v[getReg1(opcode)] != get8BitVal(opcode))
-    c->pc += 2;
+   if (c->v[getReg1(opcode)] != get8BitVal(opcode))
+      c->pc += 2;
 }
 
 void 
 OP_5XY0(Chip *c, u16 opcode) {
-  if (c->v[getReg1(opcode)] == c->v[getReg2(opcode)])
-    c->pc += 2;
+   if (c->v[getReg1(opcode)] == c->v[getReg2(opcode)])
+      c->pc += 2;
 }
 
 void 
 OP_6XNN(Chip *c, u16 opcode) {
-  c->v[getReg1(opcode)] = (u8)get8BitVal(opcode);
+   c->v[getReg1(opcode)] = (u8)get8BitVal(opcode);
 }
 
 void 
 OP_7XNN(Chip *c, u16 opcode) {
-  c->v[getReg1(opcode)] += (u8)get8BitVal(opcode);
+   c->v[getReg1(opcode)] += (u8)get8BitVal(opcode);
 }
 
 void 
 OP_8XY0(Chip *c, u16 opcode) {
-  c->v[getReg1(opcode)] = c->v[getReg2(opcode)];
+   c->v[getReg1(opcode)] = c->v[getReg2(opcode)];
 }
 
 void 
 OP_8XY1(Chip *c, u16 opcode) {
-  c->v[getReg1(opcode)] |= c->v[getReg2(opcode)];
+   c->v[getReg1(opcode)] |= c->v[getReg2(opcode)];
 }
 
 void 
 OP_8XY2(Chip *c, u16 opcode) {
-  c->v[getReg1(opcode)] &= c->v[getReg2(opcode)];
+   c->v[getReg1(opcode)] &= c->v[getReg2(opcode)];
 }
 
 void 
 OP_8XY3(Chip *c, u16 opcode) {
-  c->v[getReg1(opcode)] ^= c->v[getReg2(opcode)];
+   c->v[getReg1(opcode)] ^= c->v[getReg2(opcode)];
 }
 
 void 
 OP_8XY4(Chip *c, u16 opcode) {
-  u16 sum;
-  sum = c->v[getReg1(opcode)] + c->v[getReg2(opcode)];
-  c->v[getReg1(opcode)] = (u8)sum;
-  c->v[15] = (sum >= 0x100);
+   u16 sum;
+   sum = c->v[getReg1(opcode)] + c->v[getReg2(opcode)];
+   c->v[getReg1(opcode)] = (u8)sum;
+   c->v[15] = (sum >= 0x100);
 }
 
 void 
 OP_8XY5(Chip *c, u16 opcode) {
-  i16 sum;
-  sum = c->v[getReg1(opcode)] - c->v[getReg2(opcode)];
-  c->v[getReg1(opcode)] = (u8)sum;
-  c->v[15] = !(sum < 0);
+   if (c->v[getReg1(opcode)] > c->v[getReg2(opcode)]) {
+      c->v[15] = 1;
+   } else {
+      c->v[15] = 0;
+   }
+   c->v[getReg1(opcode)] -= c->v[getReg2(opcode)];
 }
 
 void 
 OP_8XY6(Chip *c, u16 opcode) {
-  c->v[15] = c->v[getReg2(opcode)] & 0x1;
-  c->v[getReg1(opcode)] = c->v[getReg2(opcode)] >> 1;
+   c->v[15] = c->v[getReg1(opcode)] & 0x1;
+   c->v[getReg1(opcode)] >>= 1;
 }
 
 void 
 OP_8XY7(Chip *c, u16 opcode) {
-  i16 sum;
-  sum = c->v[getReg2(opcode)] - c->v[getReg1(opcode)];
-  c->v[getReg1(opcode)] = (u8)sum;
-  c->v[15] = !(sum < 0);
+   if (c->v[getReg2(opcode)] > c->v[getReg1(opcode)]) {
+      c->v[15] = 1;
+   } else {
+      c->v[15] = 0;
+   }
+   c->v[getReg1(opcode)] = c->v[getReg2(opcode)] - c->v[getReg1(opcode)];
 }
 
 void 
 OP_8XYE(Chip *c, u16 opcode) {
-  c->v[15] = c->v[getReg2(opcode)] >> 7;
-  c->v[getReg1(opcode)] = c->v[getReg2(opcode)] << 1;
+   c->v[15] = c->v[getReg1(opcode)] >> 7;
+   c->v[getReg1(opcode)] <<= 1;
 }
 
 void 
 OP_9XY0(Chip *c, u16 opcode) {
-  if (c->v[getReg1(opcode)] != c->v[getReg2(opcode)])
-    c->pc += 2;
+   if (c->v[getReg1(opcode)] != c->v[getReg2(opcode)])
+      c->pc += 2;
 }
 
 void 
@@ -222,53 +237,50 @@ OP_ANNN(Chip *c, u16 opcode) { c->i = getAddr(opcode); }
 
 void 
 OP_BNNN(Chip *c, u16 opcode) {
-  c->pc = (c->v[0] + getAddr(opcode)) & 0x0fff;
+   c->pc = (c->v[0] + getAddr(opcode)) & 0x0fff;
 }
 
 void 
 OP_CXNN(Chip *c, u16 opcode) {
-   /*
-  time_t t;
-  srand((unsigned)time(&t));
-  c->v[getReg1(opcode)] = get8BitVal(opcode) + (rand() % 256);
-  */
+   time_t t;
+   srand((unsigned)time(&t));
+   c->v[getReg1(opcode)] = (rand() % 256) & get8BitVal(opcode);
 }
 
 void 
 OP_DXYN(Chip *c, u16 opcode) {
-  u8 x, y, n, rowData, k, l;
-  x = c->v[getReg1(opcode)] % WIDTH;
-  y = c->v[getReg2(opcode)] % HEIGHT;
-  n = (u8)get4BitVal(opcode);
+   u8 x, y, n, rowData, k, l;
+   x = c->v[getReg1(opcode)] % WIDTH;
+   y = c->v[getReg2(opcode)] % HEIGHT;
+   n = (u8)get4BitVal(opcode);
 
-  for (k = 0; k < n && y < HEIGHT; k++, y++) {
-    rowData = c->mem[c->i + k];
-
-    for (l = 0; l < 8 && (x + k) < WIDTH; l++) {
-      c->display[x + l + y * WIDTH] ^= (u8)((rowData >> (7 - l)) & 0x1);
-      c->v[15] = ((rowData >> (7 - l)) & 0x1) & (c->display[x + k + y * 64]);
-    }
-  }
+   for (k = 0; k < n && y < HEIGHT; k++, y++) {
+      rowData = c->mem[c->i + k];
+      for (l = 0; l < 8 && (x + l) < WIDTH; l++) {
+         c->display[x + l + y * WIDTH] ^= (u8)((rowData >> (7 - l)) & 0x1);
+         c->v[15] = ((rowData >> (7 - l)) & 0x1) & (c->display[x + k + y * 64]);
+      }
+   }
 }
 
 void 
 OP_EX9E(Chip *c, u16 opcode) {
-   /*
-  u8 x;
-  x = c->v[getReg1(opcode)];
-  if (x < 16 && key_pressed[x])
-    c->pc += 2;
-    */
+   SDL_Event e;
+   u8 x;
+   x = c->v[getReg1(opcode)];
+   if (x < 16 && c->kp == x) c->pc += 2;
+   /* c->kp = none; */
+   while (SDL_PollEvent(&e));
 }
 
 void 
 OP_EXA1(Chip *c, u16 opcode) {
-   /*
-  u8 x;
-  x = c->v[getReg1(opcode)];
-  if (x < 16 && !key_pressed[x])
-    c->pc += 2;
-    */
+   SDL_Event e;
+   u8 x;
+   x = c->v[getReg1(opcode)];
+   if (x < 16 && c->kp != x) c->pc += 2;
+   /* c->kp = none; */
+   while (SDL_PollEvent(&e));
 }
 
 void 
@@ -276,11 +288,33 @@ OP_FX07(Chip *c, u16 opcode) { c->v[getReg1(opcode)] = c->dtimer; }
 
 void 
 OP_FX0A(Chip *c, u16 opcode) {
-   /*
-  u8 k;
-  while (prev_key == -1)
-    continue;
-  c->v[getReg1(opcode)] = prev_key;*/
+   SDL_Event e;
+   while (SDL_PollEvent(&e));
+   while (SDL_WaitEventTimeout(&e, 50)) {
+      if (e.type == SDL_KEYDOWN) {
+         switch (e.key.keysym.sym) {
+         case SDLK_1: c->v[getReg1(opcode)] = k1; return;
+         case SDLK_2: c->v[getReg1(opcode)] = k2; return;
+         case SDLK_3: c->v[getReg1(opcode)] = k3; return;
+         case SDLK_q: c->v[getReg1(opcode)] = k4; return;
+         case SDLK_w: c->v[getReg1(opcode)] = k5; return;
+         case SDLK_e: c->v[getReg1(opcode)] = k6; return;
+         case SDLK_a: c->v[getReg1(opcode)] = k7; return;
+         case SDLK_s: c->v[getReg1(opcode)] = k8; return;
+         case SDLK_d: c->v[getReg1(opcode)] = k9; return;
+         case SDLK_z: c->v[getReg1(opcode)] = ka; return;
+         case SDLK_x: c->v[getReg1(opcode)] = k0; return;
+         case SDLK_c: c->v[getReg1(opcode)] = kb; return;
+         case SDLK_4: c->v[getReg1(opcode)] = kc; return;
+         case SDLK_r: c->v[getReg1(opcode)] = kd; return;
+         case SDLK_f: c->v[getReg1(opcode)] = ke; return;
+         case SDLK_v: c->v[getReg1(opcode)] = kf; return;
+         }
+      }
+   }
+   c->pc-=2;
+   c->kp = none; 
+   return;
 }
 
 void 
@@ -291,126 +325,120 @@ OP_FX18(Chip *c, u16 opcode) { c->stimer = c->v[getReg1(opcode)]; }
 
 void 
 OP_FX1E(Chip *c, u16 opcode) {
-  c->i = (c->i + c->v[getReg1(opcode)]) & 0x0fff;
+   c->i = (c->i + c->v[getReg1(opcode)]) & 0x0fff;
 }
 
 void 
-OP_FX29(Chip *c, u16 opcode) {}
+OP_FX29(Chip *c, u16 opcode) {
+   c->i = 0x50 + (5 * c->v[getReg1(opcode)]);
+}
 
 void 
 OP_FX33(Chip *c, u16 opcode) {
-  u16 temp, loc;
-  temp = c->v[getReg1(opcode)];
-  loc = c->i % 4096;
-  c->mem[loc] = temp / 100;
-  loc = (loc + 1) % 4096;
-  c->mem[loc] = (temp / 10) % 10;
-  loc = (loc + 1) % 4096;
-  c->mem[c->i + 2] = temp % 10;
+   u16 temp, loc;
+   temp = c->v[getReg1(opcode)];
+   loc = c->i % 4096;
+   c->mem[loc] = temp / 100;
+   loc = (loc + 1) % 4096;
+   c->mem[loc] = (temp / 10) % 10;
+   loc = (loc + 1) % 4096;
+   c->mem[c->i + 2] = temp % 10;
 }
 
 void 
 OP_FX55(Chip *c, u16 opcode) {
-  u16 tillReg, loc, j;
-  tillReg = getReg1(opcode);
-  loc = c->i;
-  for (j = 0; j <= tillReg; j++) {
-    if (loc == 4096)
-      loc = 0;
-    c->mem[loc++] = c->v[j];
-  }
-  c->i += tillReg + 1;
+   u16 tillReg, j;
+   tillReg = getReg1(opcode);
+   for (j = 0; j <= tillReg; j++) {
+      c->mem[c->i + j] = c->v[j];
+   }
 }
 
 void 
 OP_FX65(Chip *c, u16 opcode) {
-  u16 tillReg, loc, j;
-  tillReg = getReg1(opcode);
-  loc = c->i;
-  for (j = 0; j <= tillReg; j++) {
-    if (loc == 4096)
-      loc = 0;
-    c->v[j] = c->mem[loc++];
-  }
-  c->i += tillReg + 1;
+   u16 tillReg, j;
+   tillReg = getReg1(opcode);
+   for (j = 0; j <= tillReg; j++) {
+      c->v[j] = c->mem[j + c->i];
+   }
 }
 
 instrPointer Eight[8] = {
-    &OP_8XY0, &OP_8XY1, &OP_8XY2, &OP_8XY3,
-    &OP_8XY4, &OP_8XY5, &OP_8XY6, &OP_8XY7,
+   &OP_8XY0, &OP_8XY1, &OP_8XY2, &OP_8XY3,
+   &OP_8XY4, &OP_8XY5, &OP_8XY6, &OP_8XY7,
 };
 
 void 
 OP_Zero(Chip *c, u16 opcode) {
-  switch (opcode & 0x00ff) {
-  case 0xe0:
-    OP_00E0(c, opcode);
-    break;
-  case 0xee:
-    OP_00EE(c, opcode);
-    break;
-  default:
-    OP_0NNN(c, opcode);
-  }
+   switch (opcode & 0x00ff) {
+      case 0xe0:
+         OP_00E0(c, opcode);
+         break;
+      case 0xee:
+         OP_00EE(c, opcode);
+         break;
+      default:
+         OP_0NNN(c, opcode);
+   }
 }
 
 void 
 OP_Eight(Chip *c, u16 opcode) {
-  if ((opcode & 0xf) == 0xe)
-    OP_8XYE(c, opcode);
-  else
-    (*Eight[opcode & 0xf])(c, opcode);
+   if ((opcode & 0xf) == 0xe)
+      OP_8XYE(c, opcode);
+   else
+      (*Eight[opcode & 0xf])(c, opcode);
 }
 
 void 
 OP_E(Chip *c, u16 opcode) {
-  switch (opcode & 0x00ff) {
-  case 0xa1:
-    OP_EXA1(c, opcode);
-    break;
-  case 0x9e:
-    OP_EX9E(c, opcode);
-    break;
-  }
+   switch (opcode & 0x00ff) {
+      case 0xa1:
+         OP_EXA1(c, opcode);
+         break;
+      case 0x9e:
+         OP_EX9E(c, opcode);
+         break;
+   }
 }
 
 void 
 OP_F(Chip *c, u16 opcode) {
-  switch (opcode & 0x00ff) {
-  case 0x07:
-    OP_FX07(c, opcode);
-    break;
-  case 0x0a:
-    OP_FX0A(c, opcode);
-    break;
-  case 0x15:
-    OP_FX15(c, opcode);
-    break;
-  case 0x18:
-    OP_FX18(c, opcode);
-    break;
-  case 0x1e:
-    OP_FX1E(c, opcode);
-    break;
-  case 0x29:
-    OP_FX29(c, opcode);
-    break;
-  case 0x33:
-    OP_FX33(c, opcode);
-    break;
-  case 0x55:
-    OP_FX55(c, opcode);
-    break;
-  case 0x65:
-    OP_FX65(c, opcode);
-    break;
-  }
+   switch (opcode & 0x00ff) {
+      case 0x07:
+         OP_FX07(c, opcode);
+         break;
+      case 0x0a:
+         OP_FX0A(c, opcode);
+         break;
+      case 0x15:
+         OP_FX15(c, opcode);
+         break;
+      case 0x18:
+         OP_FX18(c, opcode);
+         break;
+      case 0x1e:
+         OP_FX1E(c, opcode);
+         break;
+      case 0x29:
+         OP_FX29(c, opcode);
+         break;
+      case 0x33:
+         OP_FX33(c, opcode);
+         break;
+      case 0x55:
+         OP_FX55(c, opcode);
+         break;
+      case 0x65:
+         OP_FX65(c, opcode);
+         break;
+   }
 }
 /* end of instruction set implementation ================== */
 
 instrPointer opTable[16] = {
-    OP_Zero,  OP_1NNN, OP_2NNN, OP_3XNN, OP_4XNN, OP_5XY0, OP_6XNN, OP_7XNN,
-    OP_Eight, OP_9XY0, OP_ANNN, OP_BNNN, OP_CXNN, OP_DXYN, OP_E,    OP_F,
+   OP_Zero,  OP_1NNN, OP_2NNN, OP_3XNN, OP_4XNN, OP_5XY0, OP_6XNN, OP_7XNN,
+   OP_Eight, OP_9XY0, OP_ANNN, OP_BNNN, OP_CXNN, OP_DXYN, OP_E,    OP_F,
 };
 
 int
@@ -446,13 +474,39 @@ resetChip(Chip *c) {
   c->pc = 0x200;
   c->dtimer = 0;
   c->stimer = 0;
-  c->prev_key = -1;
+  c->kp = none;
+}
+
+void
+loadFont(Chip *c) {
+   u8 fontStart, i;
+   u8 font [80] = {
+      0xF0, 0x90, 0x90, 0x90, 0xF0, /* 0 */
+      0x20, 0x60, 0x20, 0x20, 0x70, /* 1 */
+      0xF0, 0x10, 0xF0, 0x80, 0xF0, /* 2 */
+      0xF0, 0x10, 0xF0, 0x10, 0xF0, /* 3 */
+      0x90, 0x90, 0xF0, 0x10, 0x10, /* 4 */
+      0xF0, 0x80, 0xF0, 0x10, 0xF0, /* 5 */
+      0xF0, 0x80, 0xF0, 0x90, 0xF0, /* 6 */
+      0xF0, 0x10, 0x20, 0x40, 0x40, /* 7 */
+      0xF0, 0x90, 0xF0, 0x90, 0xF0, /* 8 */
+      0xF0, 0x90, 0xF0, 0x10, 0xF0, /* 9 */
+      0xF0, 0x90, 0xF0, 0x90, 0x90, /* A */
+      0xE0, 0x90, 0xE0, 0x90, 0xE0, /* B */
+      0xF0, 0x80, 0x80, 0x80, 0xF0, /* C */
+      0xE0, 0x90, 0x90, 0x90, 0xE0, /* D */
+      0xF0, 0x80, 0xF0, 0x80, 0xF0, /* E */
+      0xF0, 0x80, 0xF0, 0x80, 0x80, /* F */
+   };
+   fontStart = 0x50;
+   for (i = 0; i < 80; i++) c->mem[fontStart + i] = font[i];
 }
 
 int
 main(int argc, char** argv) {
    int quit, err;
    u16 op;
+   double curr, last;
    SDL_Event e;
    SDL_Window *window;
    SDL_Surface *surface;
@@ -464,6 +518,7 @@ main(int argc, char** argv) {
    }
 
    resetChip(&c);
+   loadFont(&c);
    err = loadProgram(&c, argv[1]);
 
    if (err) {
@@ -474,23 +529,53 @@ main(int argc, char** argv) {
    if (sdlSetup()) return 1;
    quit = 0;
 
-   /* sdl boilerplate */
-   window = SDL_CreateWindow("crisp", 
+   window = SDL_CreateWindow(
+      "crisp", 
       SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-      WIDTH*PIXELSIZE, HEIGHT*PIXELSIZE, 
-      SDL_WINDOW_SHOWN);
+      WIDTH*PIXELSIZE, HEIGHT*PIXELSIZE, SDL_WINDOW_SHOWN
+   );
    if(!window) {
       reportError();
       return 1;
    }
+
    SDL_SetWindowResizable(window, SDL_FALSE);
    surface = SDL_GetWindowSurface( window );
    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0x00, 0x00, 0x00));
    SDL_UpdateWindowSurface(window);
+   curr = 0;
+   last = 0;
 
    while (!quit) {
       while (SDL_PollEvent(&e)) {
          if (e.type == SDL_QUIT) quit = 1;
+         else if (e.type == SDL_KEYDOWN) {
+            switch (e.key.keysym.sym) {
+            case SDLK_1: c.kp  = k1; break;
+            case SDLK_2: c.kp  = k2; break;
+            case SDLK_3: c.kp  = k3; break;
+            case SDLK_q: c.kp  = k4; break;
+            case SDLK_w: c.kp  = k5; break;
+            case SDLK_e: c.kp  = k6; break;
+            case SDLK_a: c.kp  = k7; break;
+            case SDLK_s: c.kp  = k8; break;
+            case SDLK_d: c.kp  = k9; break;
+            case SDLK_z: c.kp  = ka; break;
+            case SDLK_x: c.kp  = k0; break;
+            case SDLK_c: c.kp  = kb; break;
+            case SDLK_4: c.kp  = kc; break;
+            case SDLK_r: c.kp  = kd; break;
+            case SDLK_f: c.kp  = ke; break;
+            case SDLK_v: c.kp  = kf; break;
+            }                 
+         }
+      }
+
+      curr = SDL_GetTicks();
+      if (curr - last > 16.666) {
+         last = curr - ((curr - last) - 16.666);
+         c.dtimer = c.dtimer == 0 ? 0 : c.dtimer - 1;
+         c.stimer = c.stimer == 0 ? 0 : c.stimer - 1;
       }
 
       if (c.pc > 4095) {
@@ -502,12 +587,12 @@ main(int argc, char** argv) {
       c.pc += 2;
       (*opTable[(op & 0xf000) >> 12])(&c, op);
 
-      refreshScreen(&c, surface);
-
-      SDL_UpdateWindowSurface(window);
+      refreshScreen(&c, surface, window);
+      SDL_Delay(1);
    }
 
    SDL_DestroyWindow(window);
    SDL_Quit();
    return 0;
 }
+
